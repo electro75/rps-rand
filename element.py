bf077104aa94ca413e7223bd479cf03ffddb2de5
sahil naikdhure <sanaikdhure@gmail.com>
2023-08-25 19:46:44 +0530

    add assets

--- element.py
+++ element.py
@@ -3,20 +3,20 @@ import random
 
 # all elements will have a direction they move in defined by y = mx + c
 # once they hit a boundary their equation will change (perpendicularly)
-# 
+#
+
 
 class Element:
-    def __init__(self, color, x_boundary, y_boundary):
+    def __init__(self, kind, x_boundary, y_boundary):
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
         self.x = random.randrange(0, self.x_boundary)
         self.y = random.randrange(0, self.y_boundary)
         self.size = random.randrange(4, 8)
-        self.color = color
+        self.kind = kind
 
     def __repr__(self):
-        return "Blob({}, {}, {}, {})".format(self.color, self.size, self.x, self.y)
-        
+        return "Element({}, {}, {}, {})".format(self.kind, self.size, self.x, self.y)
 
     def move(self):
         self.move_x = random.randrange(-1, 2)
--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 from element import Element
 import pygame
+
+SCISSOR = "SCISSOR"
+ROCK = "ROCK"
+PAPER = "PAPER"
+
+WHITE = (255, 255, 255)
+
+game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Blobber")
+clock = pygame.time.Clock()
+
+
+def draw_env():
+    game_display.fill(WHITE)
+
+
+def main():
+    # display code
+    # sample elements
+
+    draw_env()
+
+
+if __name__ == "__main__":
+    main()
