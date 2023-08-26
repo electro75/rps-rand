2aa8dda0e1e74c9c02cba09f7c18fe131f9def5e
Sahil Naikdhure <sanaikdhure@gmail.com>
2023-08-26 15:18:44 +0530

    show image on pygame display

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 SCISSOR = "SCISSOR"
 ROCK = "ROCK"
 PAPER = "PAPER"
+WIDTH = 800
+HEIGHT = 600
 
 WHITE = (255, 255, 255)
 
@@ -12,15 +14,27 @@ pygame.display.set_caption("Blobber")
 clock = pygame.time.Clock()
 
 
-def draw_env():
+def draw_env(img, img_rect):
     game_display.fill(WHITE)
+    game_display.blit(img, img_rect)
+    pygame.display.flip()
 
 
 def main():
     # display code
     # sample elements
 
-    draw_env()
+    myimage = pygame.image.load("assets/scissor.png")
+    imagerect = myimage.get_rect()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        draw_env(myimage, imagerect)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
