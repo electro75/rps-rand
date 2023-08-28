df25b628417e74b135b680ea84e81d63f02b68f6
Sahil Naikdhure <sanaikdhure@gmail.com>
2023-08-28 22:10:48 +0530

    image manipulation

--- main.py
+++ main.py
@@ -4,28 +4,35 @@ import pygame
 SCISSOR = "SCISSOR"
 ROCK = "ROCK"
 PAPER = "PAPER"
-WIDTH = 800
+WIDTH = 1080
 HEIGHT = 600
 
 WHITE = (255, 255, 255)
 
-game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 pygame.display.set_caption("Blobber")
 clock = pygame.time.Clock()
 
 
 def draw_env(img, img_rect):
-    game_display.fill(WHITE)
-    game_display.blit(img, img_rect)
-    pygame.display.flip()
+    screen.fill(WHITE)
+    screen.blit(img, img_rect)
+    img_rect = img_rect.move(1, -1)
+    screen.blit(img, img_rect)
+    pygame.display.update()
+    return img_rect
+    
 
 
 def main():
     # display code
     # sample elements
 
-    myimage = pygame.image.load("assets/scissor.png")
+    myimage = pygame.image.load("assets/scissor.png").convert_alpha()
+    myimage = pygame.transform.scale(myimage, (50, 50))
     imagerect = myimage.get_rect()
+    imagerect[0] = 300
+    imagerect[1] = 500
 
     while True:
         for event in pygame.event.get():
@@ -33,7 +40,7 @@ def main():
                 pygame.quit()
                 quit()
 
-        draw_env(myimage, imagerect)
+        imagerect = draw_env(myimage, imagerect)
         clock.tick(60)
 
 
